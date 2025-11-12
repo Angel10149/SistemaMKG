cc5d35fdef8e6137f4222230b1f3d53d5324240e
Angel Asencio <luisangelasencioasencio@gmail.com>
2025-11-11 20:24:10 -0500

    actualizacion de model y controller4

--- src/controller/Test.java
+++
@@ -1,9 +0,0 @@
-package controller;
-public void generarBackup(String rutaArchivo) {
-        Backup nuevo = new Backup();
-        nuevo.setIdBackup(listaBackups.size() + 1);
-        nuevo.setFecha(java.time.LocalDateTime.now().toString());
-        nuevo.setRutaArchivo(rutaArchivo);
-        nuevo.setEstado("Completado");
-        registrar(nuevo);
-    }
