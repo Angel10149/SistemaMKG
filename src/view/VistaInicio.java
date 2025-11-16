91d1090d6a61cd27ee6a2c7ce65cb2f00956ea08
Angel Asencio <luisangelasencioasencio@gmail.com>
2025-11-15 20:47:20 -0500

    actualizac12

--- src/view/VistaInicio.java
+++ src/view/VistaInicio.java
@@ -8,12 +8,12 @@ package view;
  *
  * @author KEVIN SANGAY
  */
-public class VistaLogin extends javax.swing.JFrame {
+public class VistaInicio extends javax.swing.JFrame {
 
     /**
-     * Creates new form VistaLogin
+     * Creates new form VistaInicio
      */
-    public VistaLogin() {
+    public VistaInicio() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class VistaLogin extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VistaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VistaInicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VistaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VistaInicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VistaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VistaInicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VistaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VistaInicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new VistaLogin().setVisible(true);
+                new VistaInicio().setVisible(true);
             }
         });
     }
